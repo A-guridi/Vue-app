950e4ee78fe459115729b12dd6d91da1086034b6
arturo.guridi <arturo.guridi@accenture.com>
2024-03-04 18:39:46 +0100

    Added new router and updated database

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventList.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "@/views/EventList.vue";
+import EventDetails from "@/views/EventDetails.vue";
+import About from "@/views/About.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'events-list',
-      component: EventListView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/event/:id',
-      name: 'event-details',
-      props: true, 
-      component: () => import('../views/EventDetailsView.vue')
-    },
+  routes,
+});
 
-  ]
-})
-
-export default router
+export default router;
---
+++ src/router/old_router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventList.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'events-list',
+      component: EventListView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,      // this eanbles to send info to child components
+      component: () => import('../views/EventDetailsView.vue')
+    },
+
+  ]
+})
+
+export default router
